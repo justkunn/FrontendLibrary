ad9ad21f6adc29bf2c36c33cc05f40b316aaa897
justkunn <iqbalsetiawan536@gmail.com>
2025-12-29 10:18:17 +0700

    change http to https

--- src/shared/config/env.ts
+++ src/shared/config/env.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://backendlibrary-production-0510.up.railway.app:2112";
+export const BASE_URL = "https://backendlibrary-production-0510.up.railway.app";
 // export const BASE_
